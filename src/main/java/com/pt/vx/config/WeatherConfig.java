18aa0a72cf36dace28f1dccb862421123a98111d
GinGvv <147680100+GinGvv@users.noreply.github.com>
2023-12-09 17:55:59 +0800

    WeatherConfig.java

--- src/main/java/com/pt/vx/config/WeatherConfig.java
+++ src/main/java/com/pt/vx/config/WeatherConfig.java
@@ -9,12 +9,12 @@ public class WeatherConfig {
      * 天气相关的总开关
      * true则为开启，false则为关闭
      */
-    public static boolean OPEN = false;
+    public static boolean OPEN = true;
 
     /**
      * 天气资源的key
      */
-    public static final String weatherSourceKey = "your key";
+    public static final String weatherSourceKey = "566abc572d354ae5b5a2b39687c1f364";
 
     /**
      * 天气资源类型
